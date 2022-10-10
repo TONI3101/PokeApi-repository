3f24ad9e6267fcaa37925c8bcd8f33598ee845d6
Toni3101 <antonioha3101@gmail.com>
2022-10-10 10:29:14 +0200

    10/10/2022 push

--- index.js
+++ index.js
@@ -32,15 +32,32 @@ const drawPokemon = (pokes) =>{
 
     for (const poke of pokes) {
         let div$$ = document.createElement("div");
-
+        div$$.className = "carta"
         div$$.innerHTML= `<h4>${poke.name}</h4>
-        <img src="${poke.sprites.other.home.front_default}" alt="">
+        <img class="img_poke" src="${poke.sprites.other.home.front_default}" alt="">
         <p>${poke.types.map(type => type.type.name)}</p>
         <p>weight:${poke.weight}</p>
         <p>height:${poke.height} </p>`
         gallery$$.appendChild(div$$);
     }
 
+}
+const searchPoke = (name, pokes) => {
+
+    const filteredPokes = pokes.filter((poke) => poke.name.toLowerCase().includes(name.toLowerCase()));
+    drawPokemon(filteredPokes);
+    
+
+    
+}
+
+
+const setListener = (pokes) => {
+
+    let button$$ = document.querySelector(".head-btn");
+    let input$$ = document.querySelector(".serch_poke");
+    button$$.addEventListener('click', () => searchPoke(input$$.value, pokes));
+    
 }
 
 
@@ -50,8 +67,8 @@ async function init ()  {
     console.log(mappedPokemons);
 
     drawPokemon(groupPokemon);
-
-
     
+
+    setListener(groupPokemon);
 }
 init();
